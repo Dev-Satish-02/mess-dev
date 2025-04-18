73eeff14c4230d0bfe8eb7f51d43d64d980c03ef
dev <devsatish08@gmail.com>
2025-04-18 17:18:15 +0530

    vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DayPage from "./Pages/DayPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<DayPage />} />
-        <Route path="/:day" element={<DayPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DayPage />} />
+          <Route path="/:day" element={<DayPage />} />
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </>
   );
 }
 
